5f4c27cfc731e12e11a45ef3917bad0369db4663
kunz <bboykunz@gmail.com>
2023-05-13 03:12:25 +0800

    Alter GetElement Fun and Add Display Time Feature

--- stop_watch.js
+++ stop_watch.js
@@ -1,19 +1,21 @@
 console.log("Expected construct Stop Watch JS code");
 
-let show_mg_btn = null;
+//let show_mg_btn = null;
 
-GetElement = (element) => {
-    element = document.querySelector("#show_mg");
+GetElement = (target_id) => {
+    let element = document.querySelector(`#${target_id}`);
     if (element == null) {
-        setTimeout((element) => {GetElement(element)}, 500);
+        setTimeout(() => {GetElement(target_id);}, 500);
     } else {
         element.addEventListener("click", function (e) {
             alert("Expected construct Stop Watch JS code");
         });
+        //return func;
     }
 }
 
-GetElement(show_mg_btn);
+GetElement("show_mg");
+//GetElement(show_mg_btn, "show_mg");
 
 // function GetElement(element) {
 //     return new Promise((resolve) => {
@@ -24,6 +26,28 @@ GetElement(show_mg_btn);
 // async function CheckGetElement() {
 
 // }
+PlusZero = (num) => {
+    return num >= 10 ? `${num}` : `0${num}`;
+}
 
+DisplayTime = () => {
+    let for_hour = document.querySelector("#for_hour");
+    let for_minute = document.querySelector("#for_minute");
+    let for_second = document.querySelector("#for_second");
 
+    if (for_hour == null || for_minute == null || for_second == null) {
+        return;
+    }
+
+    let dateObj = new Date();
+    for_hour.innerHTML = PlusZero(dateObj.getHours());
+    for_minute.innerHTML = PlusZero(dateObj.getMinutes());
+    for_second.innerHTML = PlusZero(dateObj.getSeconds());
+}
+
+ShowNowTime = () => {
+    DisplayTime();
+    setTimeout(() => {ShowNowTime();}, 1000);
+}
+ShowNowTime();
 
